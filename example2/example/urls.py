69c2e722600e9bd9373f485cebb0e31ad1329680
huongnguyen <huongchinguyen0202.pnv2t@gmail.com>
2014-03-14 15:05:20 +0300

    new project example2

---
+++ example2/example/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from example.myshop.views import MyOrderConfirmView
+
+from shop import urls as shop_urls
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/', include(admin.site.urls)),
+
+    url(r'^checkout/confirm/$', MyOrderConfirmView.as_view(), name='checkout_shipping'),
+    (r'^', include(shop_urls)), # <-- That's the important bit
+)
